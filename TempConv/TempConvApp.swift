7dfa8c3e3a9d3c21dc582452fc2ef551e562f718
seahpark247 <famousjedo@gmail.com>
2025-04-13 08:21:34 -0500

    Initial Commit

---
+++ TempConv/TempConvApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConvApp.swift
+//  TempConv
+//
+//  Created by Seah Park on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TempConvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TempConvTests/TempConvTests.swift
@@ -0,0 +1,17 @@
+//
+//  TempConvTests.swift
+//  TempConvTests
+//
+//  Created by Seah Park on 4/13/25.
+//
+
+import Testing
+@testable import TempConv
+
+struct TempConvTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
